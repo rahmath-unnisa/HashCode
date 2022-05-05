b7e2735b21dd9cbfbc4704520da43646dc2ca53f
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-05 20:01:18 +0530

    [Rahmath]Add: To Be Checking For Duplication

---
+++ KeyValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode
+{
+   
+        public class KeyValue<K, V>
+        {
+            public K key { get; set; }
+            public V value { get; set; }
+        }
+    
+}
